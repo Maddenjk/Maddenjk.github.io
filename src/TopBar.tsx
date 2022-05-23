5ec408abbf848f3076105d54a5714fb3189713d5
Maddenjk <34973190+Maddenjk@users.noreply.github.com>
2022-05-23 12:35:02 -0400

    Update TopBar.tsx

--- src/TopBar.tsx
+++ src/TopBar.tsx
@@ -11,8 +11,7 @@ function TopBar() {
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="./DiceRoller">Dice Roller</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
