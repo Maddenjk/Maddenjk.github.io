575b907536b2662af339ce4092c19ea657c5138c
Maddenjk <34973190+Maddenjk@users.noreply.github.com>
2022-05-23 06:00:44 -0400

    Move build to main

---
+++ precache-manifest.b718e0e11bb95f4e28490362fa88e40c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d3714bc28de463e78d00c3cdd9539b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3423ff602752e0833ac6",
+    "url": "/static/css/2.5544c887.chunk.css"
+  },
+  {
+    "revision": "3423ff602752e0833ac6",
+    "url": "/static/js/2.b67276df.chunk.js"
+  },
+  {
+    "revision": "63094b6617fd8eaec95e",
+    "url": "/static/js/main.f696a63c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
